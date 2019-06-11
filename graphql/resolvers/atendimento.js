83a5fc7ce555ae44bd9f8d98d6e868e7efb96872
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2019-06-11 14:08:42 -0300

    aula 6.3

--- graphql/resolvers/atendimento.js
+++ graphql/resolvers/atendimento.js
@@ -1,15 +1,10 @@
 const Operacoes = require('../../infraestrutura/operations')
+
 const Atendimentos = new Operacoes('atendimento')
 
 const resolvers = {
-    Query: {
-        atendimentos: () => Atendimentos.lista(),
-        atendimento: (root, { id }) => Atendimentos.buscaPorId(id) 
-    },
     Mutation: {
-        adicionarAtendimento: (root, params) => Atendimentos.adiciona(params),
-        atualizarAtendimento: (root, params) => Atendimentos.atualiza(params),
-        deletarAtendimento: (root, { id }) => Atendimentos.deleta(id)
+        adicionarAtendimento: (root, params) => Atendimentos.adiciona(params)
     }
 }
 
