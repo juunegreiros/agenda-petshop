fcbf34d6152712e5027bf04d41bae4780e458650
ju <ju_bsnegreiros@hotmail.com>
2019-07-01 14:11:19 -0300

    Adiciona cors

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const consign = require('consign')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 module.exports = () => {
   const app = express()
   
   app.use(bodyParser.urlencoded({extended: true}))
   app.use(bodyParser.json())
-
+  app.use(cors())
   consign()
     .include('controllers')
     .into(app)
